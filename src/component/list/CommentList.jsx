5a7d638ccdf38f853e59f68d6dd034d2176041b6
sums-up <starirof@naver.com>
2023-10-19 08:08:40 +0900

    [23-10-19-목] 예제 코드 push

---
+++ src/component/list/CommentList.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import {CommentListItem} from "./CommentListItem";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  
+  & > * {
+    :not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
+`;
+
+export const CommentList = (props) => {
+    const { comments } = props;
+
+    return (
+        <Wrapper>
+            {comments.map((comment, index) => {
+                return <CommentListItem key={comment.id} comment={comment}/>;
+            })}
+        </Wrapper>
+    );
+}
---
+++ src/component/ui/TextInput.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTextarea = styled.textarea`
+  width: calc(100% - 32px);
+  ${(props) =>
+    props.height && `height: ${props.height}px`}
+  padding: 16px;
+  font-size: 16px;
+  line-height: 20px;
+`;
+
+export const TextInput = (props) => {
+    const { height, value, onChange } = props;
+
+    return <StyledTextarea height={height} value={value} onChange={onChange}/>;
+}
